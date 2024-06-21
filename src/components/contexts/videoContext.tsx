d12c47b2b5f003650850391af5f5b913559f382a
trash <73361653+galenddavis@users.noreply.github.com>
2024-06-21 17:47:45 -0400

    updated context to check for undefined default video

--- src/components/contexts/videoContext.tsx
+++ src/components/contexts/videoContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { Video, videoList } from "../utils/vid-utils"
 
 const defaultVideo: Video = {
@@ -6,4 +6,16 @@ const defaultVideo: Video = {
     id: videoList[0].id
 }
 
-export const VideoContext = createContext({defaultVideo})
+export const VideoContext = createContext<Video | undefined>(defaultVideo)
+
+
+// Checks to ensure VideoContext is not undefined. Throws error if so.  
+// eslint-disable-next-line react-refresh/only-export-components
+export const useVideoContext = () => {
+    const video = useContext(VideoContext);
+    if (video === undefined) {
+        throw new Error("VideoContext is undefined");
+    }
+    return video;
+}
+
