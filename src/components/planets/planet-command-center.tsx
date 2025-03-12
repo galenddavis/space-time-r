814ffdf173f521c85274e56e01ae5b1973862028
trash <73361653+galenddavis@users.noreply.github.com>
2025-03-12 18:18:31 -0400

    added baseline planet animation

--- src/components/planets/planet-command-center.tsx
+++ src/components/planets/planet-command-center.tsx
@@ -1,10 +1,11 @@
-import planetImg from "../../assets/25minPlanet.png"
+// import planetImg from "../../assets/25minPlanet.png"
+import planetImg from "../../assets/25minPlanet.svg"
 
 
 const PlanetCommander = () => {
     return (
         <div className="planetary-command-wrapper">
-            <img src={planetImg} alt="" className="planet-img" />
+            <img src={planetImg} alt="" className="planet-img active-planet" />
         </div>
     );
 }
