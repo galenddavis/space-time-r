3806677680387c0c004320f3f599317e6ce60bbb
trash <73361653+galenddavis@users.noreply.github.com>
2025-04-10 21:45:31 -0400

    added last planet and set duration and asset link logic

--- src/components/planets/planet-command-center.tsx
+++ src/components/planets/planet-command-center.tsx
@@ -1,13 +1,49 @@
 // import planetImg from "../../assets/25minPlanet.png"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import planetImg from "../../assets/25minPlanet.svg"
-// import planetImg2 from "../../assets/15minPlanet.svg"
+import planetImg2 from "../../assets/15minPlanet.svg"
+import planetImg3 from "../../assets/5minPlanet.svg"
 
+interface PlanetCommanderProps {
+    timerDuration: "short" | "medium" | "long"
+}
 
-const PlanetCommander = () => {
+const PlanetCommander = ({ timerDuration }: PlanetCommanderProps) => {
 
     const [animationRunning, setAnimationRunning] = useState<boolean>(false)
-    // const [currPlanet, setCurrPlanet] = useState<string>(planetImg)
+    const [currPlanet, setCurrPlanet] = useState<string>(planetImg)
+
+
+
+    useEffect(() => {
+        const modeDurations: Record<'short' | 'medium' | 'long', number> = {
+            short: 300,   // 5 minutes as seconds
+            medium: 900,  // 15 minutes as seconds
+            long: 1500,   // 25 minutes as seconds
+          };
+        const planet = document.getElementById("planet-img")
+        const duration = modeDurations[timerDuration] + "s"
+        if (planet) {
+            planet.style.animationDuration = duration
+
+        }
+        const setPlanet = () => {
+            switch (timerDuration) {
+                case "short":
+                    setCurrPlanet(planetImg)
+                    break;
+                case "medium":
+                    setCurrPlanet(planetImg2)
+                    break;
+                case "long":
+                    setCurrPlanet(planetImg3)
+                    break;
+                default:
+                    break;
+            }
+        }
+        setPlanet()
+    },[timerDuration])
 
     const planet = document.getElementById("planet-img")
     if (planet) {
@@ -27,7 +63,7 @@ const PlanetCommander = () => {
 
     return (
         <div className="planetary-command-wrapper boardered">
-            <img src={planetImg} alt="" id="planet-img" className="planet-img active-planet" />
+            <img src={currPlanet} alt="" id="planet-img" className="planet-img active-planet" />
         </div>
     );
 }
--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect, SetStateAction } from 'react';
 // import "./timer.css"
 
-const Timer: React.FC = () => {
+
+interface TimerProps {
+  timerDuration: 'short' | 'medium' | 'long',
+  setTimerDuration: React.Dispatch<SetStateAction<'short' | 'medium' | 'long'>>
+
+}
+
+const Timer = ({timerDuration, setTimerDuration}: TimerProps) => {
   const [seconds, setSeconds] = useState<number>(1500); // 25 minutes as seconds
-  const [mode, setMode] = useState<'short' | 'medium' | 'long'>('long');
+  const [workCount, setWorkCount] = useState<number>(0);
 
   
+  // Effect to set the timer duration based on the selected mode
+  // This effect runs whenever the mode changes and sets the seconds accordingly
   useEffect(() => {
     const modeDurations: Record<'short' | 'medium' | 'long', number> = {
         short: 300,   // 5 minutes as seconds
@@ -13,25 +22,35 @@ const Timer: React.FC = () => {
         long: 1500,   // 25 minutes as seconds
       };
 
-    setSeconds(modeDurations[mode]);
-  }, [mode]);
+    setSeconds(modeDurations[timerDuration]);
+  }, [timerDuration]);
 
+  // Timer effect
+  // This effect runs every second and decrements the timer
   useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>;
-    if (seconds === 0) {
-      // Timer expired
-      console.log('Timer expired!');
-    } else {
-      timer = setTimeout(() => {
+    const timer = setInterval(() => {
+      if (seconds > 0) {
         setSeconds(seconds - 1);
-      }, 1000);
-    }
-
-    return () => clearTimeout(timer);
-  }, [seconds]);
+      } else { //Timer Expires
+        if (timerDuration === 'long' && workCount === 4) {
+          setTimerDuration('medium');
+          setWorkCount(workCount + 1);
+        } else if (timerDuration === 'long' && workCount < 4) {
+          setTimerDuration('short');
+          setWorkCount(workCount + 1);
+        } else if (timerDuration === 'medium') {
+          setTimerDuration('long');
+          setWorkCount(0)
+        } else if (timerDuration === 'short') {
+          setTimerDuration('long');
+        }
+      }
+    }, 1000)
+      return () => clearInterval(timer);
+  }, [seconds, setTimerDuration, timerDuration, workCount]);
 
   const handleModeChange = (newMode: 'short' | 'medium' | 'long') => {
-    setMode(newMode);
+    setTimerDuration(newMode);
   };
 
   const formatTime = (timeInSeconds: number): string => {
